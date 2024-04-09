bd0ac59e89c37fd00c195e5d5d028ace22cad062
Tracy Chimita <nellyachieng88@gmail.com>
2024-04-09 11:23:01 +0300

    js-acting-on -events-lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+      const leftNumbers = dodger.style.left.replace("px", "");
+      const left = parseInt(leftNumbers, 10);
+  
+      dodger.style.left = `${left - 1}px`;
+    }
+  });
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
